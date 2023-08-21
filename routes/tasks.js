e3745881ab32ad56cd033f9c502039b35e3f2381
Synchx <hatta.asnan00@gmail.com>
2023-08-21 23:05:02 +0700

    section & list of tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -44,4 +44,67 @@ route.put('/edit/:taskid', async(req, res) => {
     }
 });
 
+// Add section
+route.post('/add-section', async (req, res) => {
+    try {
+        const sectionName = req.body.sectionName; // Get the section name from the request body
+
+        const { data, error } = await supabase.from('Section')
+            .insert([{ sectionName: sectionName }])
+            .select(); // Insert the new section into the Section table
+
+        if (error) {
+            throw error;
+        }
+
+        res.send(`Section "${sectionName}" has been created with ID ${data[0].SectionID}.`);
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('An error occurred while creating the section.');
+    }
+});
+
+// View list of tasks
+route.get('/tasks', async (req, res) => {
+    try {
+        const { data, error } = await supabase.from('Task')
+            .select('taskID, taskName, taskDueDate, taskPriority, taskPIC, taskDesc'); // Select the required fields
+
+        if (error) {
+            throw error;
+        }
+
+        res.json(data);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('An error occurred while fetching the tasks.');
+    }
+});
+
+// Delete a task
+route.delete('/delete-task/:taskid', async (req, res) => {
+    try {
+        const { taskid } = req.params;
+
+        const { data, error } = await supabase.from('Task')
+            .delete()
+            .eq('taskID', taskid);
+
+        if (error) {
+            throw error;
+        }
+
+        if (data.length === 0) {
+            res.status(404).send('Task not found.');
+        } else {
+            res.send('Task deleted successfully.');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('An error occurred while deleting the task.');
+    }
+});
+
+
 export default route;
